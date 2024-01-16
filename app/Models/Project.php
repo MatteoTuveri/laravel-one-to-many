a03809ae9aa3d55d4075f6efaa9cc81347c72f55
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2024-01-16 12:14:28 +0100

    Add correlation user-projects, add categories rcms and add correlation projects-categories

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -19,4 +19,14 @@ class Project extends Model
         }
         return $slug;
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+{
+    return $this->belongsTo(Category::class);
+}    
 }
